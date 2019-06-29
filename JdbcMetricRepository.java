7a595e153cebbffff88fec30f3996da7a49f0575
oxylix <a.hakkou@gmail.com>
2019-06-29 14:32:23 +0200

    Remove old file

--- JdbcMetricRepository.java
+++
@@ -1,9 +0,0 @@
-package com.front.ReactiveGateway.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.front.ReactiveGateway.model.JdbcMetric;
-
-public abstract interface JdbcMetricRepository  extends CrudRepository<JdbcMetric, Long>{
-	
-}
--- ThPMetricRepository.java
+++
@@ -1,9 +0,0 @@
-package com.front.ReactiveGateway.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.front.ReactiveGateway.model.ThPMetric;
-
-public abstract interface ThPMetricRepository  extends CrudRepository<ThPMetric, Long> {
-
-}
